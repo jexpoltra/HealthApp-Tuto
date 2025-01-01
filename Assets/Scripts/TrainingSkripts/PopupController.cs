d38ace98f3799a05f9a6dcefb03400f4b23753e9
samah mrili <132463301+jexpoltra@users.noreply.github.com>
2025-01-01 18:11:13 +0100

    Some changes in Training page

--- Assets/Scripts/TrainingSkripts/PopupController.cs
+++ Assets/Scripts/TrainingSkripts/PopupController.cs
@@ -1,6 +1,18 @@
 using UnityEngine;
 
-public class PopupController
+public class ExercisePopupController : MonoBehaviour
 {
-    
+    public GameObject PopupPanel; // Referenz zum Popup-Panel
+
+    // Methode, um das Popup zu öffnen
+    public void ShowPopup()
+    {
+        PopupPanel.SetActive(true); // Popup anzeigen
+    }
+
+    // Methode, um das Popup zu schließen
+    public void ClosePopup()
+    {
+        PopupPanel.SetActive(false); // Popup ausblenden
+    }
 }
